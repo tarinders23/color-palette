32e4714d10a465797dececf9a4152800a09181e6
Tarinder <tarinders23@gmail.com>
2024-05-05 20:37:06 +0530

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # color-palette
+
+# Steps to Run
+1. Create Venv
+```
+python3 -m venv venv
+source venv/bin/activate
+```
+
+2. install
+
+```pip3 install -r requirements.txt```
+3. Run 
+
+```
+Add CHATGPT_API_KEY value in config.py
+python3 -m sanic server.app --host 0.0.0.0
+```
+4. Open
+```http://0.0.0.0:8000/```
--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 from sanic import Sanic, redirect
-from tortoise.contrib.sanic import register_tortoise
 
 from app.middleware import add_cors_headers
 from app.options import setup_options
-from config import DB_CONFIG, APP_NAME
+from config import APP_NAME
 
 app = Sanic(name=APP_NAME)
 app.config.FALLBACK_ERROR_FORMAT = "json"
@@ -19,31 +18,9 @@ for route in app.router.routes_all:
 
 @app.route('/', methods=["GET"])
 async def handler(request):
-    return redirect('/public/index.html')
+    return redirect('/public/index.html?user_id=tarinder')
 
 
 app.register_listener(setup_options, "before_server_start")
 # app.register_middleware(authenticate_user, "request")
 app.register_middleware(add_cors_headers, "response")
-# Setup Database
-# register_tortoise(app, DB_CONFIG, generate_schemas=True)
-
-###### Tortoise logging ############
-# fmt = logging.Formatter(
-#     fmt="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s",
-#     datefmt="%Y-%m-%d %H:%M:%S",
-# )
-# sh = logging.StreamHandler(sys.stdout)
-# sh.setLevel(logging.DEBUG)
-# sh.setFormatter(fmt)
-#
-# # will print debug sql
-# logger_db_client = logging.getLogger("db_client")
-# logger_db_client.setLevel(logging.DEBUG)
-# logger_db_client.addHandler(sh)
-#
-# logger_tortoise = logging.getLogger("tortoise")
-# logger_tortoise.setLevel(logging.DEBUG)
-# logger_tortoise.addHandler(sh)
-# Run App
-# app.run(host="0.0.0.0", port=8000, debug=True)
