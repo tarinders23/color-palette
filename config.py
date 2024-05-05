77ac09384ef686d1a08dab246ac422911839fa36
Tarinder <tarinders23@gmail.com>
2024-05-05 20:24:21 +0530

    config values

--- config.py
+++ config.py
@@ -4,21 +4,19 @@ from datetime import timedelta
 REGION = "eu-west-3"
 # Backend BASE URL
 
-APP_NAME = "PLATFORM"
+APP_NAME = "COLOR_PALETTE"
 AUTH_TOKEN_EXPIRY = timedelta(days=30)
+CHATGPT_API_KEY = ''
 DB_CONFIG = dict(
     connections=dict(
         default=dict(
             engine='tortoise.backends.asyncpg',
             credentials=dict(
-                host="websamp-eu.cg2gmo2o12rn.eu-west-3.rds.amazonaws.com",
+                host="",
                 port="5432",
-                # user="stag_user",
-                # password="stag_pass",
-                # database="websamp_stag"
-                user="vikas",
-                password="vikas-pass",
-                database="websamp_prod"
+                user="",
+                password="",
+                database=""
             )
         ),
         sqlite="sqlite://testfile.db"
